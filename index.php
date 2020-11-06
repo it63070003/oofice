af1615d70868eee534e7b5f6fc9baf4a95cc90e7
it63070003 <73647989+it63070003@users.noreply.github.com>
2020-11-06 10:44:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OOFice</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="img/oof/head.jpg" class="img-fluid" width="100%"/>
+                <h1>Wel Come To OOFice!</h1>
+                <p>Hello there welcome to oofice where is the office for oofer!</p>
+                <br>
+                <p>Please take a look inside our oofice</p>
+                <br>
+                <p>We also have a lot of Robux if you're working hard and also have punishment if you don't work or oof See ya!</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                            <img src="img/content/office2.jpg" class="img-fluid" width="100%"/>
+                        </div>
+                        <div class="row">
+                            <img src="img/oof/spooder.png" class="img-fluid" width="100%"/>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <img src="img/oof/hq2.png" class="img-fluid" width="100%"/>
+                        <h1>Spooder!</h1>
+                        <p>Hello i'm spooder the manager of office, i can shoot web on your wall if you don't do work!</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <img src="img/oof/hq.jpg" class="img-fluid" width="100%"/>
+                    </div>
+                    <div class="col-lg-6">
+                        <img src="img/work/work3.jpg" class="img-fluid" width="100%"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
